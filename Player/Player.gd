57b0325b5f5d0cb8d53ff6a32572db3263cc3ab9
alptekinkarabacak <alptekinkarabacak@gmail.com>
2023-11-19 18:11:18 +0300

    Character animations and the background are added

--- Player/Player.gd
+++ Player/Player.gd
@@ -15,21 +15,31 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	
+	if velocity.y > 0:
+		animation.play("Fall")
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		animation.play("jump")
 		velocity.y = JUMP_VELOCITY
 		animation.play("Jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction == -1:
+		get_node("AnimatedSprite2D").flip_h = true;
+	elif direction == 1:
+		get_node("AnimatedSprite2D").flip_h = false;
+	
 	if direction:
-		animation.play("Run")
 		velocity.x = direction * SPEED
+		if velocity.y == 0:
+			animation.play("Run")
 	else:
-		animation.play("Idle")
+		if velocity.y == 0:
+			animation.play("Idle")
+		
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
